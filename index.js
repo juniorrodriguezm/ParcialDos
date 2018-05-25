887598a563077e49d2f6bc1de9f8c96812bf23cf
Fernando Junior Rodriguez Marin <1113688678@academico.icesi.edu.co>
2018-05-25 10:55:02 -0500

    Estilos y plantillas

--- index.js
+++ index.js
@@ -1,5 +1,19 @@
-const express = require('express');
+const express = require('express'), engines = require('consolidate');;
 const app = express();
 app.use('/css',express.static( 'public/css'));
 
+app.engine('hbs', engines.handlebars);
+
+app.set('views', './views');
+app.set('view engine', 'hbs');
+
+app.use(express.static('public'));
+
 app.listen(5000, () => console.log('Example app listening on port 5000!'));
+
+/*Esta parte es para cargar las paginas*/
+app.get('/', (req, res) => {
+    
+        res.render('index');
+    
+    });
